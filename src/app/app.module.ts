98e2b0f7f415e415103fe9f19c841d0fb39ab9cb
at-vudang <ngocvupct1995@gmail.com>
2017-07-31 17:10:27 +0700

    Update template v2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { TEMPLATECOMPONENTS } from './component/template';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TEMPLATECOMPONENTS
   ],
   imports: [
     BrowserModule
