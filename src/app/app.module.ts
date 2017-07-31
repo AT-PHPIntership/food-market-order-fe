48a0056720ab63228a587ee0266c426a51ce359b
at-vudang <ngocvupct1995@gmail.com>
2017-07-31 17:15:41 +0700

    Layout home v2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TEMPLATECOMPONENTS } from './component/template';
+import { HOMECOMPONENTS } from './component/home';
 @NgModule({
   declarations: [
-    AppComponent, TEMPLATECOMPONENTS
+    AppComponent, TEMPLATECOMPONENTS, HOMECOMPONENTS
   ],
   imports: [
     BrowserModule
