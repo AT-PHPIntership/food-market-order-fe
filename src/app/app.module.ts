69166563f1fe2f890219dce6120578c01588e6d3
at-vudang <ngocvupct1995@gmail.com>
2017-08-03 11:11:08 +0700

    Update template v2.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {TemplateComponents} from './component/template';
 @NgModule({
--- src/app/component/template/index.ts
+++ src/app/component/template/index.ts
@@ -11,4 +11,4 @@ export const TemplateComponents = [
   HeaderComponent,
   PaginationComponent,
   NotFoundComponent
-]
+];
