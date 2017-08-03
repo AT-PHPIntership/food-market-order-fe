712cb0fe466e979d2c3e73eeaec4279784ebf195
at-vudang <ngocvupct1995@gmail.com>
2017-08-03 10:19:09 +0700

    Update template v2.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TemplateComponents } from './component/template';
+import { HeaderComponent } from './component/template/header/header.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { NotFoundComponent } from './component/template/not-found/not-found.component';
 @NgModule({
   declarations: [
-    AppComponent, TemplateComponents
+    AppComponent, HeaderComponent, FooterComponent, NotFoundComponent
   ],
   imports: [
     BrowserModule
