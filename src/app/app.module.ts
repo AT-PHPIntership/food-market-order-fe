bc584f08ed7e3c2d16d92539a99afc81ef8f107f
at-vudang <ngocvupct1995@gmail.com>
2017-08-03 10:23:35 +0700

    Update template v2.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './component/template/header/header.component';
-import { FooterComponent } from './component/template/footer/footer.component';
-import { NotFoundComponent } from './component/template/not-found/not-found.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './component/template/header/header.component';
+import {FooterComponent} from './component/template/footer/footer.component';
+import {NotFoundComponent} from './component/template/not-found/not-found.component';
+import {PaginationComponent} from './component/template/pagination/pagination.component';
+import {BreadcrumbsComponent} from './component/template/breadcrumbs/breadcrumbs.component';
+
 @NgModule({
-  declarations: [
-    AppComponent, HeaderComponent, FooterComponent, NotFoundComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        NotFoundComponent,
+        PaginationComponent,
+        BreadcrumbsComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
