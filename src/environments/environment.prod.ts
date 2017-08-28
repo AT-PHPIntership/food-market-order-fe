b3c3599c43dd1f860e6bbafb1e75c3fdfeed9ec6
at-vudang <ngocvupct1995@gmail.com>
2017-08-28 09:24:21 +0700

    Update file environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlToken: 'http://192.168.33.10/oauth/token',
+  hostname: 'http://192.168.33.10',
   client_secret: '9qVYHF8PXUJUKTv0pXPdfbdCeQ3m2BnuMBccW8PQ',
   client_id: 2,
 };
