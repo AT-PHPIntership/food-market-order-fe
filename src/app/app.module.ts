cd1f7afeadf98c392ebbb095a4b5096342a43f79
at-dungvan <dungvan2512@gmail.com>
2017-08-30 13:43:44 +0700

    v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './component/template';
 import { routing } from './app.route';
 import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component'
+
 import { SharedModule } from './shared/shared.module';
 @NgModule({
     declarations: [
         AppComponent,
         TemplateComponent,
-        LoginComponent
+        LoginComponent,
+        RegisterComponent
     ],
     imports: [
         routing,
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NotFoundComponent } from './component/template/not-found/not-found.component';
 import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component' ;
+
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundComponent }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
