f393fe423d05e3e87b5a322a4c14296e1c486323
hungnguyen <hungbkdn1295@gmail.com>
2017-09-01 10:59:10 +0700

    Fix convention

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,12 +6,12 @@ import 'rxjs/Rx';
 @Injectable()
 export class APIService {
 
-	constructor(private http: Http) { }
+    constructor (private http: Http) { }
 
-	apiGet = (url, condition_value) => {
-		let url_request = url.toString() + ((condition_value == null) ? '' : ("/" + condition_value.toString()));
-		return this.http
-			.get(url_request)
-			.map(res => res.json());
-	}
+    apiGet = (url, condition_value) => {
+        url_request = url.toString() + ((condition_value == null) ? '' : ('/' + condition_value.toString()));
+        return this.http
+            .get(url_request)
+            .map(res => res.json());
+    }
 }
