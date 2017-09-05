3d4a333845aa6b998625a7f7dfce25bcbc0c4947
at-vudang <ngocvupct1995@gmail.com>
2017-09-05 09:47:16 +0700

    Pagination

--- src/app/component/template/pagination/pagination.component.ts
+++ src/app/component/template/pagination/pagination.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationService } from '../../../service/pagination.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
-
-  constructor() { }
-
+  pagination: PaginationService;
+  id: number;
+  private sub: any;
+  constructor(private paginationService: PaginationService,
+              private route: ActivatedRoute) {
+    this.pagination = this.paginationService;
+  }
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    });
   }
 
 }
