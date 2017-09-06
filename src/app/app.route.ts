002ffe05b4ab72e4897856d927a1bbcd8a159240
hungnguyen <hungbkdn1295@gmail.com>
2017-09-06 08:25:33 +0700

    update home

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './component/template/not-found/not-found.component';
 import { LoginComponent } from './component/login/login.component';
+import { CategoryComponent } from './component/category/category.component';
 import { ListfoodComponent } from './component/listfood/listfood.component';
 import { RegisterComponent } from './component/register/register.component';
 import { BreadcrumbsComponent } from './component/template/breadcrumbs/breadcrumbs.component';
@@ -9,7 +10,7 @@ import { LoggedGuard } from './security/logged.guard';
 import { NoLoggedGuard } from './security/no-logged.guard';
 
 const appRoutes: Routes = [
-  { path: 'home', component: BreadcrumbsComponent },
+  { path: 'home', component: CategoryComponent },
   { path: 'foods', component: ListfoodComponent },
   { path: 'login', component: LoginComponent, canActivate: [NoLoggedGuard], data: {
     breadcrumb: 'login'
